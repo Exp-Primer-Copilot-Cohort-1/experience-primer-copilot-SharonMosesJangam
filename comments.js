3d5bad13ca40991d6ffc54111540ed0592e1028c
SharonMosesJangam <110843736+SharonMosesJangam@users.noreply.github.com>
2024-02-27 04:27:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,4 +1,4 @@
-// Create a web server that listens on port 3000 and serves the following JSON data when it receives a GET request to the path /comments:
+// Create web server
 
 // {
 //     "comments": [
